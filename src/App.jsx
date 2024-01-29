6242cbc7433a912c42e1f5c73088eab84974da71
NatNichols <natalie.e.nichols@gmail.com>
2024-01-29 16:55:30 -0500

    stub up VendingMachine component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import VendingMachine from './VendingMachine'
 
 const App = () => {
   return ( 
     <div>
-      
+      <VendingMachine />
     </div>
   )
 }
---
+++ src/VendingMachine.jsx
@@ -0,0 +1,7 @@
+const VendingMachine = () => {
+  return ( 
+    <h1>Hello Vending Machine World</h1>
+  )
+}
+
+export default VendingMachine;
